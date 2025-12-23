9c401ee82e4cdfa3bc26ca884ac07a6c924a6703
sara <sara@gmail.com>
2025-12-23 15:53:00 +0530

    updatevar

--- var.tf
+++ var.tf
@@ -2,7 +2,7 @@ variable "AWS_REGION" {
     default = "ap-southeast-1"
 }
 variable "AWS_AMI" {
-    default = "ami-0435fcf800fb5418d"
+    default = "ami-05f071c65e32875a8"
 }
 variable "AWS_AMI_UBUNTU" {
     default = "ami-02c7683e4ca3ebf58"
@@ -17,7 +17,7 @@ variable "skey" {
     default = ""
 }
 variable "pkey" {
-    default = "Linux-Singapore-Key-Pair"
+    default = "singapore-linux-key-pair"
 }
 variable "rpass" {
     default = "111"
